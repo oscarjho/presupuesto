aa1aa638d595c2ad6d1a286828127b75825eaafc
Oscar Hernandez <oscarjavierho@gmail.com>
2021-05-24 14:14:34 -0430

    Adding pregunta component

---
+++ src/components/Pregunta.js
@@ -0,0 +1,43 @@
+import React, { Fragment, useState } from "react";
+
+const Pregunta = () => {
+  // definir el state
+  const [cantidad, setCantidad] = useState(0);
+  const [error, setError] = useState(false);
+
+  // funcion que lee el presupuesto
+  const definirPresupuesto = (e) => {
+    setCantidad(parseInt(e.target.value, 10));
+  };
+  // submit para definir el presupuesto
+  const agregarPresupuesto = (e) => {
+    e.preventDefault();
+    // validar
+    if (cantidad < 1 || isNaN(cantidad)) {
+      setError(true);
+      return;
+    }
+    //que hacer si pasa la validacion
+  };
+
+  return (
+    <Fragment>
+      <h2> Coloca Tu Presupuesto </h2>
+      <form onSubmit={agregarPresupuesto}>
+        <input
+          type="number"
+          className="u-full-width"
+          placeholder="Coloca tu presupuesto"
+          onChange={definirPresupuesto}
+        />
+        <input
+          type="submit"
+          className="button-primary u-full-width"
+          value="Definir Presupuesto"
+        />
+      </form>
+    </Fragment>
+  );
+};
+
+export default Pregunta;
