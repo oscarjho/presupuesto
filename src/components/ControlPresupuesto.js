9c9e3559f8d100cdf1dc0c418e9f62e1a4547f8d
Oscar Hernandez <oscarjavierho@gmail.com>
2021-06-05 14:30:45 -0430

    Cambiando el color del restante

--- src/components/ControlPresupuesto.js
+++ src/components/ControlPresupuesto.js
@@ -1,10 +1,13 @@
 import React, { Fragment } from "react";
+import { revisarPresupuesto } from "../helpers";
 
 const Presupuesto = ({ presupuesto, restante }) => {
   return (
     <Fragment>
       <div className="alert alert-primary">Presupuesto $ {presupuesto}</div>
-      <div className="alert">Restante ${restante}</div>
+      <div className={revisarPresupuesto(presupuesto, restante)}>
+        Restante ${restante}
+      </div>
     </Fragment>
   );
 };
